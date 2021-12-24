cd9b153228510c464f134b754e2007eeaf66769e
coinlordd <35095979+coinlordd@users.noreply.github.com>
2021-12-24 01:05:18 +0100

    Update index.js

--- src/components/Portal/index.js
+++ src/components/Portal/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { isMobile } from 'react-device-detect'
 import styled, { keyframes } from 'styled-components'
 
 const Rotate = keyframes`
@@ -28,16 +29,43 @@ const Circle = styled.div`
 
 const Glare = styled(Circle)`
   animation: ${Rotate} 8s linear infinite;
-  transform-origin: 50% 50%;
   background: rgba(13, 18, 29, 0.05);
   box-shadow: inset 0px 10px 10px rgba(255, 255, 255, 0.3);
 `
 
+const MobileCircle = () => {
+  return (
+    <svg width="700" height="530" viewBox="0 0 700 530" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_i_72_445)">
+        <circle cx="350" cy="350" r="350" fill="#0D121D"/>
+      </g>
+      <defs>
+        <filter id="filter0_i_72_445" x="0" y="0" width="700" height="710" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+        <feOffset dy="10"/>
+        <feGaussianBlur stdDeviation="5"/>
+        <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.28 0 0 0 0 1 0 0 0 0.2 0"/>
+        <feBlend mode="normal" in2="shape" result="effect1_innerShadow_72_445"/>
+        </filter>
+      </defs>
+    </svg>
+  )
+}
+
 export const Portal = () => {
   return (
     <Wrapper>
-      <Circle/>
-      <Glare/>
+      {isMobile ? (
+        <MobileCircle/>
+      ) : (
+        <>
+          <Circle/>
+          <Glare/>
+        </>
+      )}
     </Wrapper>
   )
 }
